4d88279c887f34f52c2968cc4e3497643eae4694
vwolfley <vern.wolfley@gmail.com>
2014-11-19 16:54:37 -0700

    removed windowVM

--- config.js
+++ config.js
@@ -33,7 +33,7 @@
 
 var appConfig = new function() {
 
-    this.Version = "v1.3.0 | 11/19/2014";
+    this.Version = "v1.3.1 | 11/19/2014";
 
     this.ArcGISInstanceURL = "http://geo.azmag.gov/gismag/rest";
     //this.exportWebMapUrl = "http://geo.azmag.gov/gismag/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task";  // Generic Print Service
