2653c036febe754910448780521954c37312b457
Tony.Urquidi <tony.urquidi@gisinc.com>
2019-09-26 07:45:34 -0600

    AMD cbr module

--- Gruntfile.js
+++ Gruntfile.js
@@ -193,7 +193,7 @@ module.exports = function (grunt) {
         concat: {
             js: {
                 src: ["dist/app/js/**", "!dist/app/js/generateClassBreaks.js", "!dist/app/js/main.js", "!dist/app/js/app.js",
-                        "!dist/app/js/layerlist.js","!dist/app/js/sidebar.js"],
+                        "!dist/app/js/layerlist.js","!dist/app/js/sidebar.js", "!dist/app/js/maps/cbr.js"],
                 dest: jsFilePath
             },
             css: {
