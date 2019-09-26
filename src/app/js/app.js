2b38280918fb044072231f77ea1cc5ce1564d3e2
Tony.Urquidi <tony.urquidi@gisinc.com>
2019-09-26 14:37:04 -0600

    amd widgets home

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -8,7 +8,8 @@ define([
     "mag/maps/colorRamps",
     "mag/maps/cbr",
     "mag/maps/maps",
-    "mag/utilities"
+    "mag/utilities",
+    "mag/widgets/home"
 ], function () {
 
     /** The global MAG object. 
