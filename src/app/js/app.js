1391ba7af549895594060192a0d9798b04994374
Tony.Urquidi <tony.urquidi@gisinc.com>
2019-09-27 12:07:12 -0600

    amd reports/customGeographyReports

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -8,7 +8,8 @@ define([
     "mag/maps/colorRamps",
     "mag/maps/cbr",
     "mag/maps/maps",
-    "mag/utilities"
+    "mag/utilities",
+    "mag/reports/customGeographyReports"
 ], function () {
 
     /** The global MAG object. 
