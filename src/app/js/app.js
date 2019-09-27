5a46520df0c1a36cfe6fc7510b37541da8b78c00
Tony.Urquidi <tony.urquidi@gisinc.com>
2019-09-27 12:19:30 -0600

    amd reports/reportCharts

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -8,7 +8,8 @@ define([
     "mag/maps/colorRamps",
     "mag/maps/cbr",
     "mag/maps/maps",
-    "mag/utilities"
+    "mag/utilities",
+    "mag/reports/reportCharts"
 ], function () {
 
     /** The global MAG object. 
