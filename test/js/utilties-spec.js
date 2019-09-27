48f4990ee1f34dd30a057417b3ef43d530ec50df
Zebadiah K. Steeby <zsteeby@gmail.com>
2019-09-27 10:31:28 -0500

    Add utilties test spec and remove generic test spec.

---
+++ test/js/utilties-spec.js
@@ -0,0 +1,9 @@
+define([
+    'mag/utilities'
+], function (utilties) {
+    describe('Simple Test', function () {
+        it('Should be equal', function () {
+            expect(1).toEqual(1);
+        })
+    })
+})
