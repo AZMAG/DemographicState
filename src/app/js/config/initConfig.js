e35b9278c746b46e929b811677eb1c1286f5fe17
Tony.Urquidi <tony.urquidi@gisinc.com>
2019-10-03 16:04:51 -0600

    exported all functions except the parsing of the url params since its done in utils

--- src/app/js/config/initConfig.js
+++ src/app/js/config/initConfig.js
@@ -7,97 +7,104 @@ define([
     config
 ){
 
-
-
-    var _parseURL = function(){
+    var _parseURL= function(){
         var initStr = utils.qs("init");
         if (initStr) {
             return JSON.parse(initStr);
         }
     };
 
-    var _objectExists = function(data, obj){
-        if (typeof data !== 'undefined' && typeof data[obj] !== 'undefined'){
-            return true;
-        }
-        else  {
-            return false;
-        }
+    var initConfig = {
+        parsedData: _parseURL(),
+        getExtent: function(){
+            return this.returnObject(this.parsedData, 'extent')
+        },
+        getPanel: function(){
+            return this.returnObject(this.parsedData, 'panel')
+        },
+        getTransparency: function(){
+            return this.returnObject(this.parsedData, 'transparency')
+        },
+        getLegend: function(){
+            return this.returnObject(this.parsedData, 'legend')
+        },
+        updateLayers: function(){
+            if(this._objectExists(this.parsedData, 'visibleLayers')){
+                this._updateConfig(this.parsedData.visibleLayers);
+            }
 
-    };
-
-    var _updateConfig = function(layers) {
-        layers.forEach(function(layer) {
-            config.layers.forEach(function (conf){
-                if (layer === conf.id) {
-                    conf.visible = true;
+        },
+        mapDataFieldNameMatches: function(name){
+            if (this._objectExists(this.parsedData, 'mapData')){
+                if (this.parsedData.mapData.FieldName === name){
+                    return true;
                 }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return false;
+            }
+        },
+        checkBasemap: function(map) {
+            if (this._objectExists(this.parsedData, 'basemap')) {
+                if (this.parsedData.basemap === map) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                return null;
+            }
+        },
+        _objectExists: function(data, obj){
+            if (typeof data !== 'undefined' && data.hasOwnProperty(obj)){
+                return true;
+            }
+            else  {
+                return false;
+            }
+
+        },
+        _updateConfig: function(layers) {
+            layers.forEach(function(layer) {
+                config.layers.forEach(function (conf){
+                    if (layer === conf.id) {
+                        conf.visible = true;
+                    }
+                })
             })
-        })
 
-    }
-
-    var updateLayers = function(){
-        if(_objectExists(parsedData, 'visibleLayers')){
-            _updateConfig(parsedData.visibleLayers);
-        }
-
-    };
-
-    var mapDataFieldNameMatches = function(name){
-        if (_objectExists(parsedData, 'mapData')){
-            if (parsedData.mapData.FieldName === name){
-                return true;
+        },
+        returnObject: function(data, obj){
+            if (this._objectExists(data, obj)){
+                return data[obj];
             }
             else {
-                return false;
+                return null;
             }
         }
-        else {
-            return false;
-        }
+        // extent: this.returnObject(this.parsedData, 'extent'), 
+        // panel: this.returnObject(this.parsedData, 'panel'), 
+        // transparency: this.returnObject(this.parsedData, 'transparency'), 
+        // legend: this.returnObject(this.parsedData, 'legend'),        
+
     }
 
+    
+    return initConfig;
+        // {
+        //     mapDataFieldNameMatches: mapDataFieldNameMatches,
+        //     updateLayers: updateLayers,
+        //     checkBasemap: checkBasemap,
+        //     extent: extent,
+        //     panel: panel,
+        //     transparency: transparency,
+        //     legend: legend
 
-    var checkBasemap = function(map) {
-        if (_objectExists(parsedData, 'basemap')) {
-            if (parsedData.basemap === map) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            return 'not defined';
-        }
-    }
-
-    var return_object = function(data, obj){
-        if (_objectExists(data, obj)){
-            return data[obj];
-        }
-        else {
-            return false;
-        }
-    }
-
-    var parsedData = _parseURL();
-    var extent = return_object(parsedData, 'extent'); 
-    var panel = return_object(parsedData, 'panel'); 
-    var transparency = return_object(parsedData, 'transparency'); 
-    var legend = return_object(parsedData, 'legend'); 
-
-
-    return {
-        mapDataFieldNameMatches: mapDataFieldNameMatches,
-        updateLayers: updateLayers,
-        checkBasemap: checkBasemap,
-        extent: extent,
-        panel: panel,
-        transparency: transparency,
-        legend: legend
-
-    }; 
+        // }; 
 
 });
