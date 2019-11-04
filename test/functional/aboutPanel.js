ec284088ef409af5804b56b23368ee66d1bdb1f8
Zebadiah K. Steeby <zsteeby@gmail.com>
2019-11-04 15:45:27 -0600

    Update functional tests

--- test/functional/aboutPanel.js
+++ test/functional/aboutPanel.js
@@ -1,15 +1,15 @@
 var { registerSuite } = intern.getPlugin('interface.object');
 var { assert } = intern.getPlugin('chai');
-    registerSuite('About', {
-        'about opens': function () {
+    registerSuite('About Panel', {
+        'panel opens': function () {
             return this.remote
             .get('src/index.html')
-            .findById('sidebar')
             .findByCssSelector('[panel-target="about-view"]')
             .moveMouseTo(39, 18)
             .clickMouseButton(0)
-            .then(function(val) {                
-                assert.equal(1, 1);
+            .getVisibleText()
+            .then(text => {
+                assert.equal(text, 'About');
             });
         }
     });
